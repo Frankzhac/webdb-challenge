0ee6d87b4c464793884384d943eb40d4d9f1d3e1
Frankzhac <Frank.hemle@gmail.com>
2019-06-14 13:03:42 -0400

    get actions model

--- index.js
+++ index.js
@@ -92,6 +92,20 @@ server.delete('/projects/:id', async (req, res) => {
   } catch (error) {}
 });
 
+// Actions Model
+// Get actions
+server.get('/actions', async (req, res) => {
+  // get the projects from the database
+  try {
+    const projects = await db('projects'); // all the records from the table
+    res.status(200).json(projects);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+
+
 const port = process.env.PORT || 5000;
 server.listen(port, () =>
   console.log(`\n** API running on http://localhost:${port} **\n`)
