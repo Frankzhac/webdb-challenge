a67a2c61bc7a46d9a123f5b244b7b11ab3aa1d59
Frankzhac <Frank.hemle@gmail.com>
2019-06-14 12:27:25 -0400

    server set up

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = {
+  client: 'sqlite3',
+  connection: {
+    filename: './data/projects.db3',
+  },
+  useNullAsDefault: true, // needed for sqlite
+};
+const db = knex(knexConfig);
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
